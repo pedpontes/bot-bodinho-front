428e27eadb5498c32da43fa6eb257a7fde1b756a
unknown <pontespeter@gmail.com>
2025-12-03 23:07:55 -0300

    feat: handle http request baseurl

--- src/helpers/http.ts
+++ src/helpers/http.ts
@@ -1,9 +1,22 @@
 import axios from "axios";
 
 const PORT = 8080;
-export const BASE_URL = `http://localhost:${PORT}`;
+const useLocalhost = true;
+
+export let BASE_URL: string | null;
 
 function httpHelper() {
+  switch (location.hostname) {
+    case "bot-bodinho-front.vercel.app":
+      BASE_URL = "https://bodinho-discord.ddns.net";
+    default:
+      if (useLocalhost) {
+        BASE_URL = `http://${location.hostname}:${PORT}`;
+        break;
+      }
+      BASE_URL = "https://bodinho-discord.ddns.net";
+  }
+
   const instance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL || BASE_URL,
     withCredentials: true,
